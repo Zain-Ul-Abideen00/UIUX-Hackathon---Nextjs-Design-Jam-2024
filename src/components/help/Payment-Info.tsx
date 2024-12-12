c19d5b3ef8e3989e27a0e03635434b024b1cd84c
Zain UL Abideen <164017250+Zain-Ul-Abideen00@users.noreply.github.com>
2024-12-12 21:53:52 +0500

    Update Payment-Info SEO

--- src/components/help/Payment-Info.tsx
+++ src/components/help/Payment-Info.tsx
@@ -14,14 +14,14 @@ export default function PaymentInfo() {
 
       <div className="space-y-4 pl-4">
         <p>Visa, Mastercard, Diners Club, Discover, American Express, Visa Electron, Maestro</p>
-        <p>If you enter your PAN information at checkout, you'll be able to pay for your order with PayTM or a local credit or debit card.</p>
+        <p>If you enter your PAN information at checkout, you&apos;ll be able to pay for your order with PayTM or a local credit or debit card.</p>
         <p>Apple Pay</p>
       </div>
 
       <div className="space-y-4">
         <p>
           Nike Members can store multiple debit or credit cards in their profile for faster checkout. 
-          If you're not already a Member, join us today.
+          If you&apos;re not already a Member, join us today.
         </p>
         
         <div className="flex gap-4">
